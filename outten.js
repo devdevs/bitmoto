f8f2793acb34de93e98f6b6af29739877427bb2f
Devin <devinpeterson10@gmail.com>
2017-01-10 15:08:01 -0500

    Create outten.js

---
+++ outten.js
@@ -0,0 +1,63 @@
+// Devs JS for Outten Auto Group
+
+
+//Outten Auto Group
+if(document.domain == "www.outtencars.com"){
+
+}
+
+//Outten Kia Hamburg
+if(document.domain == "www.outtenkia.com"){
+
+}
+
+//Outten Chevrolet Allentown
+if(document.domain == "www.outtenchevyallentown.com"){
+
+}
+
+
+if(document.URL.indexOf("ford-fusion.htm") >= 0){ 
+document.getElementById('BitMoto-FordFusionLease').innerHTML = '270';
+document.getElementById('BitMoto-FordFusionDAS').innerHTML = '270';
+//document.getElementById('BitMoto-FordFusionAPR').innerHTML = '72';
+//document.getElementById('BitMoto-FordFusionMSRP').innerHTML = '22,120';
+//document.getElementById('BitMoto-SalesImage').src = 'http://www.strunkmarketing.com/bitmoto/ford/images/sales/current.jpg';
+document.getElementById('BitMoto-FordDisclaimerDate').innerHTML = '3/31/17';
+}
+
+if(document.URL.indexOf("ford-focus.htm") >= 0){ 
+document.getElementById('BitMoto-FordFocusLease').innerHTML = '228';
+document.getElementById('BitMoto-FordFocusDAS').innerHTML = '228';
+//document.getElementById('BitMoto-FordFocusAPR').innerHTML = '72';
+//document.getElementById('BitMoto-FordFocusMSRP').innerHTML = '16,775';
+//document.getElementById('BitMoto-SalesImage').src = 'http://www.strunkmarketing.com/bitmoto/ford/images/sales/current.jpg';
+document.getElementById('BitMoto-FordDisclaimerDate').innerHTML = '3/31/17';
+}
+
+if(document.URL.indexOf("ford-escape.htm") >= 0){ 
+document.getElementById('BitMoto-FordEscapeLease').innerHTML = '249';
+document.getElementById('BitMoto-FordEscapeDAS').innerHTML = '249';
+//document.getElementById('BitMoto-FordEscapeAPR').innerHTML = '72';
+//document.getElementById('BitMoto-FordEscapeMSRP').innerHTML = '23,600';
+//document.getElementById('BitMoto-SalesImage').src = 'http://www.strunkmarketing.com/bitmoto/ford/images/sales/current.jpg';
+document.getElementById('BitMoto-FordDisclaimerDate').innerHTML = '3/31/17';
+}
+
+if(document.URL.indexOf("ford-explorer.htm") >= 0){ 
+document.getElementById('BitMoto-FordExplorerLease').innerHTML = '338';
+document.getElementById('BitMoto-FordExplorerDAS').innerHTML = '338';
+//document.getElementById('BitMoto-FordExplorerAPR').innerHTML = '72';
+//document.getElementById('BitMoto-FordExplorerMSRP').innerHTML = '31,660';
+//document.getElementById('BitMoto-SalesImage').src = 'http://www.strunkmarketing.com/bitmoto/ford/images/sales/current.jpg';
+document.getElementById('BitMoto-FordDisclaimerDate').innerHTML = '3/31/17';
+}
+
+if(document.URL.indexOf("ford-f150.htm") >= 0){ 
+document.getElementById('BitMoto-FordF150Lease').innerHTML = '394';
+document.getElementById('BitMoto-FordF150DAS').innerHTML = '394';
+//document.getElementById('BitMoto-FordF150APR').innerHTML = '72';
+//document.getElementById('BitMoto-FordF159MSRP').innerHTML = '26,540';
+//document.getElementById('BitMoto-SalesImage').src = 'http://www.strunkmarketing.com/bitmoto/ford/images/sales/current.jpg';
+document.getElementById('BitMoto-FordDisclaimerDate').innerHTML = '3/31/17';
+}
