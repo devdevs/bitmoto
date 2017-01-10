5776bac83cf61ed84742c236adc3162804e217f1
Devin <devinpeterson10@gmail.com>
2017-01-10 16:06:38 -0500

    Update bitmotoTest.js

--- bitmotoTest.js
+++ bitmotoTest.js
@@ -1,7 +1,7 @@
 
 
 
-if(document.domain == "www.bitmotomarketing.com"){
+if(document.domain == "bitmotomarketing.com"){
   
 document.getElementById('contact-form').onclick = "ga('form', 'submit')";
 document.getElementsByClassName('wp-image-293').src = 'https://raw.githubusercontent.com/devdevs/bitmoto/master/Generic-EarlyBirdProgramBanner.png';
