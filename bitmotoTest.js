8a031972954b639881427aecd9c9c7af8e07a588
Devin <devinpeterson10@gmail.com>
2017-01-23 10:11:37 -0500

    Delete bitmotoTest.js

--- bitmotoTest.js
+++
@@ -1,9 +0,0 @@
-
-
-
-if(document.domain == "bitmotomarketing.com"){
-  
-document.getElementById('contact-form').onclick = "ga('form', 'submit')";
-document.getElementsByClassName('wp-image-293').src = 'https://raw.githubusercontent.com/devdevs/bitmoto/master/Generic-EarlyBirdProgramBanner.png';
-
-}
