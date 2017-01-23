f4bd08dc02cd959e372498485e68b8af3f40ee07
Devin <devinpeterson10@gmail.com>
2017-01-23 09:01:15 -0500

    Create bitmoto.js

---
+++ bitmoto.js
@@ -0,0 +1,50 @@
+// Devs JS for Bitmoto Clients
+
+// Outten | Carman | Coccia
+
+
+
+// Outten Chevy Allentown
+if(document.domain == "www.outtenchevyallentown.com"){
+
+}
+
+
+
+// Outten Kia Hamburg
+if(document.domain == "www.outtenkia.com"){
+
+}
+
+
+
+// Coccia Ford Wilkes-Barre
+if(document.domain == "www.cocciaford.com"){
+
+}
+
+
+
+// Coccia Lincoln Wilkes-Barre
+if(document.domain == "www.coccialincoln.com"){
+
+}
+
+
+
+// Carman Fiat New Castle
+if(document.domain == "www.fiatusaofnewcastle.com"){
+
+}
+
+
+
+
+if(document.URL.indexOf("ford-fusion.htm") >= 0){ 
+document.getElementById('BitMoto-FordFusionLease').innerHTML = '270';
+document.getElementById('BitMoto-FordFusionDAS').innerHTML = '270';
+//document.getElementById('BitMoto-FordFusionAPR').innerHTML = '72';
+//document.getElementById('BitMoto-FordFusionMSRP').innerHTML = '22,120';
+//document.getElementById('BitMoto-SalesImage').src = 'http://www.strunkmarketing.com/bitmoto/ford/images/sales/current.jpg';
+document.getElementById('BitMoto-FordDisclaimerDate').innerHTML = '3/31/17';
+}
