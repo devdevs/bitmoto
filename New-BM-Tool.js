6932bf751dd00feffbc5ef388a02185f38d0401f
Devin <devinpeterson10@gmail.com>
2017-08-15 14:47:38 -0400

    Update New-BM-Tool.js

--- New-BM-Tool.js
+++ New-BM-Tool.js
@@ -2,12 +2,11 @@
 
 if(document.domain == "www.strunkmarketing.com"){ var dealer = "3"; }
 if(document.domain == "www.haldemanfordallentown.com"){ var dealer = "4"; }
-if(document.domain == "www.dunphyford.com"){ var dealer = "4"; }
-
+if(document.domain == "www.dunphyford.com"){ var dealer = "7"; }
 
 if(document.URL.indexOf("ford-focus") >= 0){ var model = "Focus"; }
 if(document.URL.indexOf("ford-fusion") >= 0){ var model = "Fusion"; }
-if(document.URL.indexOf("strunk") >= 0){ var model = "Escape"; }
+if(document.URL.indexOf("ford-escape") >= 0){ var model = "Escape"; }
 if(document.URL.indexOf("ford-explorer") >= 0){ var model = "Explorer"; }
 if(document.URL.indexOf("ford-f150") >= 0){ var model = "F150"; }
 if(document.URL.indexOf("ford-edge") >= 0){ var model = "Edge"; }
@@ -27,84 +26,128 @@ var spreedsheet = "1txaR1PBvsV4yQmCS3LhevWJm6SuLAUFf4iC7_uxiq0g";
 // END - INITIATE SHEETS API
 
 
-// START - DEALER SPECIFIC VEHICLE PRICING
-
-var range = ""+model+"!A"+dealer+":P"+dealer+"";
-var url = "https://sheets.googleapis.com/v4/spreadsheets/"+spreedsheet+"/values/"+range+"?key="+key;
-
-$.getJSON(url, function(data) {
-    $.each(data.values, function(index,value) {
-	if(document.querySelector('#BitMoto-Ford'+model+'Lease') !== null)  { document.getElementById('BitMoto-Ford'+model+'Lease').innerHTML = value[Lease]; }
-	if(document.querySelector('#BitMoto-Ford'+model+'MSRP') !== null)  { document.getElementById('BitMoto-Ford'+model+'MSRP').innerHTML = value[MSRP]; }
-	if(document.querySelector('#BitMoto-Ford'+model+'DAS') !== null)  { document.getElementById('BitMoto-Ford'+model+'DAS').innerHTML = value[DASDown]; }
-	if(document.querySelector('#BitMoto-DASDownText') !== null)  { document.getElementById('BitMoto-DASDownText').innerHTML = value[DASDownText]; }
-	if(document.querySelector('#BitMoto-Ford'+model+'APR') !== null)  { document.getElementById('BitMoto-Ford'+model+'APR').innerHTML = value[APR]; }
-	if(document.querySelector('#BitMoto-Ford'+model+'Term') !== null)  { document.getElementById('BitMoto-Ford'+model+'Term').innerHTML = value[Term]; }
-	if(document.getElementById('BitMoto-FordCurrentSaleSmallBanner') !== null)  { document.getElementById('BitMoto-FordCurrentSaleSmallBanner').src = value[TopBannerImageURL]; }
-	if(document.getElementById('BitMoto-FordCurrentSaleURL') !== null)  { document.getElementById('BitMoto-FordCurrentSaleURL').href = value[TopBannerLinkURL]; }
-	if(document.getElementById('BitMoto-Video') !== null) { document.getElementById('BitMoto-Video').src = value[PlusOfferText]; }
-    });
-});
-
-
-
-
-// END - DEALER SPECIFIC VEHICLE PRICING
-
 
 // START - DEALER SPECIFIC INFORMATION
+
+function DealerInfo() {
 
 var range = "A"+dealer+":P"+dealer+"";
 var url = "https://sheets.googleapis.com/v4/spreadsheets/"+spreedsheet+"/values/"+range+"?key="+key;
 
 $.getJSON(url, function(data) {
     $.each(data.values, function(index,value) {
+    
 	if(document.querySelector('#BitMoto-Phone') !== null)  { document.getElementById('BitMoto-Phone').innerHTML = value[1]; }
+	
     });
 });
+
+}
 
 // END - DEALER SPECIFIC INFORMATION
 
 
+
 // START - MANUFACTURER LEVEL VEHICLE PRICING (will use manufacturer values if dealer-specific values are blank)
 
-function OEMdefaults () {
+function ManufacturerPricing() {
 
-var range = ""+model+"!A2:P2";
+var range = model+"!A2:Q2";
 var url = "https://sheets.googleapis.com/v4/spreadsheets/"+spreedsheet+"/values/"+range+"?key="+key;
 
 $.getJSON(url, function(data) {
     $.each(data.values, function(index,value) {
+   
 	if(document.querySelector('#BitMoto-Ford'+model+'Lease') !== null)  {
-		if( document.getElementById('BitMoto-Ford'+model+'Lease').innerHTML == "") {
-			document.getElementById('BitMoto-Ford'+model+'Lease').innerHTML = value[4];
-		}
+		document.getElementById('BitMoto-Ford'+model+'Lease').innerHTML = value[Lease];
 	}
 	if(document.querySelector('#BitMoto-Ford'+model+'DAS') !== null)  {
-		if( document.getElementById('BitMoto-Ford'+model+'DAS').innerHTML == "") {
-			document.getElementById('BitMoto-Ford'+model+'DAS').innerHTML = value[5];
-		}
+		document.getElementById('BitMoto-Ford'+model+'DAS').innerHTML = value[DASDown];
 	}
 	if(document.querySelector('#BitMoto-Ford'+model+'MSRP') !== null)  {
-		if( document.getElementById('BitMoto-Ford'+model+'MSRP').innerHTML == "") {
-			document.getElementById('BitMoto-Ford'+model+'MSRP').innerHTML = value[1];
-		}
+		document.getElementById('BitMoto-Ford'+model+'MSRP').innerHTML = value[MSRP];
 	}
 	if(document.querySelector('#BitMoto-DASDownText') !== null)  {
-		if( document.getElementById('BitMoto-DASDownText').innerHTML == "") {
-			document.getElementById('BitMoto-DASDownText').innerHTML = value[6];
-		}
+		//if( document.getElementById('BitMoto-DASDownText').innerHTML == "") {
+			document.getElementById('BitMoto-DASDownText').innerHTML = value[DASDownText];
+		//}
 	}
 	if(document.querySelector('#BitMoto-Ford'+model+'APR') !== null)  {
-		if( document.getElementById('BitMoto-Ford'+model+'APR').innerHTML == "") {
-			document.getElementById('BitMoto-Ford'+model+'APR').innerHTML = value[7];
+		//if( document.getElementById('BitMoto-Ford'+model+'APR').innerHTML == "") {
+			document.getElementById('BitMoto-Ford'+model+'APR').innerHTML = value[APR];
+		//}
+	}
+	if(document.querySelector('#BitMoto-Ford'+model+'Months') !== null)  {
+		if( document.getElementById('BitMoto-Ford'+model+'Months').innerHTML == "") {
+			document.getElementById('BitMoto-Ford'+model+'Months').innerHTML = value[Term];
 		}
+	}
+	if(document.querySelector('#BitMoto-'+model+'Video') !== null)  {
+			document.getElementById('BitMoto-'+model+'Video').src = value[VideoURL];
 	}
     });
 });
 
 }
 
-window.onload = OEMdefaults;
-
 // END - MANUFACTURER LEVEL VEHICLE PRICING
+
+
+
+
+
+
+// START - DEALER SPECIFIC VEHICLE PRICING
+
+function DealerPricing() {
+
+var range = model+"!A"+dealer+":Q"+dealer;
+var url = "https://sheets.googleapis.com/v4/spreadsheets/"+spreedsheet+"/values/"+range+"?key="+key;
+
+$.getJSON(url, function(data) {
+    $.each(data.values, function(index,value) {
+    
+	if(document.querySelector('#BitMoto-Ford'+model+'Lease') !== null)  { 
+		if (value[Lease] !== "" && value[Lease] !== undefined) { document.getElementById('BitMoto-Ford'+model+'Lease').innerHTML = value[Lease]; }
+	}
+	
+	if(document.querySelector('#BitMoto-Ford'+model+'MSRP') !== null)  {
+		if (value[MSRP] !== "" && value[MSRP] !== undefined) { document.getElementById('BitMoto-Ford'+model+'MSRP').innerHTML = value[MSRP]; }
+	}
+	
+	if(document.querySelector('#BitMoto-Ford'+model+'DAS') !== null)  {
+		if (value[DASDown] !== "" && value[DASDown] !== undefined) { document.getElementById('BitMoto-Ford'+model+'DAS').innerHTML = value[DASDown]; }
+	}
+	
+	if(document.querySelector('#BitMoto-DASDownText') !== null)  {
+		if (value[DASDownText] !== "" && value[DASDownText] !== undefined) { document.getElementById('BitMoto-DASDownText').innerHTML = value[DASDownText]; }
+	}
+	
+	if(document.querySelector('#BitMoto-Ford'+model+'APR') !== null)  {
+		if (value[APR] !== "" && value[APR] !== undefined) { document.getElementById('BitMoto-Ford'+model+'APR').innerHTML = value[APR]; }
+	}
+	
+	if(document.querySelector('#BitMoto-Ford'+model+'Months') !== null)  {
+		if (value[Term] !== "" && value[Term] !== undefined) { document.getElementById('BitMoto-Ford'+model+'Months').innerHTML = value[Term]; }
+	}
+	
+	if(document.getElementById('BitMoto-FordCurrentSaleSmallBanner') !== null)  {
+		if (value[TopBannerImageURL] !== "" && value[TopBannerImageURL] !== undefined) { document.getElementById('BitMoto-FordCurrentSaleSmallBanner').src = value[TopBannerImageURL]; }
+	}
+	
+	if(document.getElementById('BitMoto-FordCurrentSaleURL') !== null)  { document.getElementById('BitMoto-FordCurrentSaleURL').href = value[TopBannerLinkURL]; }
+	//if(document.getElementById('BitMoto-'+model+'Video') !== null) { document.getElementById('BitMoto-'+Model+'Video').src = value[VideoURL]; }
+    });
+});
+
+}
+
+// END - DEALER SPECIFIC VEHICLE PRICING
+
+
+ManufacturerPricing();
+DealerInfo();
+DealerPricing();
+window.onload = DealerPricing();
+setTimeout(DealerPricing,2000);
+setTimeout(DealerPricing,6000);
