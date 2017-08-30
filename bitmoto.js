2be5dca864862d7577782ee26f656add2dea2858
Devin <devinpeterson10@gmail.com>
2017-08-30 16:47:11 -0400

    Update bitmoto.js

--- bitmoto.js
+++ bitmoto.js
@@ -113,13 +113,8 @@ if (!document.getElementById(cssId))
 // Gilboy Ford Whitehall
 if(document.domain == "www.gilboyford.com"){
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-105686635-1', 'auto');
-  ga('send', 'pageview');
+  ga('create', 'UA-105686635-1', 'auto', 'BitMoTracker');
+  ga('BitMoTracker.send', 'pageview');
 
 }
 
