386c7b582e641c88154388cb9d8cde7bfa1fc076
Devin <devinpeterson10@gmail.com>
2017-12-01 17:15:32 -0500

    Update bitmoto.js

--- bitmoto.js
+++ bitmoto.js
@@ -1,6 +1,6 @@
 // Devs JS for Bitmoto Clients
 
-// Outten | Carman | Coccia | Pacifico | Gilboy | Sloan
+// Outten | Carman | Coccia | Pacifico | Gilboy | Sloan | Dunphy
 
 
 // Carman Ford New Castle
@@ -396,6 +396,54 @@ if(document.domain == "www.carman-ford.com"){
 	
 }
 // END - Carman Ford New Castle
+
+
+
+// START - Dunphy Ford Philadelphia
+if(document.domain == "www.dunphyford.com"){
+/*
+	if(document.querySelector('#BitMoto-FordEscapeLease') !== null)  { document.getElementById('BitMoto-FordEscapeLease').innerHTML = '165'; }
+	if(document.querySelector('#BitMoto-FordEscapeDAS') !== null)  { document.getElementById('BitMoto-FordEscapeDAS').innerHTML = '3740'; }
+
+	if(document.querySelector('#BitMoto-FordFusionLease') !== null)  { document.getElementById('BitMoto-FordFusionLease').innerHTML = '165'; }
+	if(document.querySelector('#BitMoto-FordFusionDAS') !== null)  { document.getElementById('BitMoto-FordFusionDAS').innerHTML = '3620'; }
+
+	if(document.querySelector('#BitMoto-FordFocusLease') !== null)  { document.getElementById('BitMoto-FordFocusLease').innerHTML = '139'; }
+	if(document.querySelector('#BitMoto-FordFocusDAS') !== null)  { document.getElementById('BitMoto-FordFocusDAS').innerHTML = '2589'; }
+
+	if(document.querySelector('#BitMoto-FordExplorerLease') !== null)  { document.getElementById('BitMoto-FordExplorerLease').innerHTML = '215'; }
+	if(document.querySelector('#BitMoto-FordExplorerDAS') !== null)  { document.getElementById('BitMoto-FordExplorerDAS').innerHTML = '3485'; }
+
+	if(document.querySelector('#BitMoto-FordF150Lease') !== null)  { document.getElementById('BitMoto-FordF150Lease').innerHTML = '249'; }
+	if(document.querySelector('#BitMoto-FordF150DAS') !== null)  { document.getElementById('BitMoto-FordF150DAS').innerHTML = '3569'; }
+	
+	if(document.querySelector('#BitMoto-FordEdgeLease') !== null)  { document.getElementById('BitMoto-FordEdgeLease').innerHTML = '255'; }
+	if(document.querySelector('#BitMoto-FordEdgeDAS') !== null)  { document.getElementById('BitMoto-FordEdgeDAS').innerHTML = '3254'; }
+*/
+	if(document.querySelector('#BitMoto-ExtraDisclaimer') !== null)  {
+	
+		if(document.URL.indexOf("ford-fusion.htm") >= 0){
+			document.getElementById('BitMoto-ExtraDisclaimer').innerHTML = '17MY Fusion. FWD SE 200A. MSRP $25,710. Lease for $165/mo w/ $3,620 DAS. 36 month lease with 10,500 miles/year. **$5,884 total savings. Total cash includes $2,150 retail customer cash + $500 Ford Credit retail bonus cash on 2.0/2.7L Engines + $1,244 average dealer discount + $990 package discount. Security deposit waived. Taxes, tags, and $249 documentation fee due at signing. Expires 11/30/2017 See dealer for details.';
+		}
+		if(document.URL.indexOf("ford-focus.htm") >= 0){
+			document.getElementById('BitMoto-ExtraDisclaimer').innerHTML = '17MY Focus. 4DR SE 200A w/AUTO. MSRP $20,145. Lease for $139/mo w/ $2,589 DAS. 36 month lease with 10,500 miles/year. **Includes all available rebates and average dealer discount. Security deposit waived. Taxes, tags, and doc fee extra. Expires 11/30/2017. See dealer for details.';
+		}
+		if(document.URL.indexOf("ford-escape.htm") >= 0){
+			document.getElementById('BitMoto-ExtraDisclaimer').innerHTML = '17 MY Escape. FWD SE 200A. MSRP $26,145. Lease for $165/mo w/ $3,740 DAS. 36 month lease with 10,500 miles/year. **Includes all available rebates and average dealer discount. Security deposit waived.Taxes, tags, and doc fee extra. Expires 11/30/2017. See dealer for details.';
+		}
+		if(document.URL.indexOf("ford-explorer.htm") >= 0){
+			document.getElementById('BitMoto-ExtraDisclaimer').innerHTML = '17MY Explorer. FWD Base 100A. MSRP $32,605. Lease for $215/mo w/ $3,485 DAS. 36 month lease with 10,500 miles/year. **Includes all available rebates and average dealer discount. Security deposit waived. Taxes, tags, and doc fee extra. Expires 11/30/2017 See dealer for details.';
+		}
+		if(document.URL.indexOf("ford-f150.htm") >= 0){
+			document.getElementById('BitMoto-ExtraDisclaimer').innerHTML = '17MY F150 4x4 SuperCab XL Sport. 101A +861 w/ 2.7L EcoBoost. MSRP $41,090. Lease for $249/mo w/ $3,569 DAS. 36 month lease with 10,500 miles/year. **$10,984 Total Savings includes $1,250 retail customer cash + $250 retail bonus cash + $1,250 Ford Credit retail customer cash + $1,500 open bonus cash (XL/XLT) + $1,250 retail customer cash (XLT) + $2,984 average dealer discount + $2,500 MSRP package discount. Taxes & fees extra. Security deposit waived. Taxes, tags, and doc fee extra. Expires 11/30/2017. See dealer for details.';
+		}
+		if(document.URL.indexOf("ford-edge.htm") >= 0){
+			document.getElementById('BitMoto-ExtraDisclaimer').innerHTML = '17MY Edge. AWD SEL 200A. MSRP $34,680. Lease for $255/mo w/ $3,254 DAS. 36 month lease with 10,500 miles/year. **Includes all available rebates and average dealer discount. Security deposit waived.Taxes, tags, and doc fee extra. Expires 11/30/2017. See dealer for details.';
+		}
+	}
+
+}
+// END - Dunphy Ford Philadelphia
 
 
 // -------- END ------------ INDIVIDUAL FORD DEALER VEHICLES ------------------
