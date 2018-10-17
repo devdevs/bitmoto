5d850ecd159ca38c525155665110b37ca75399a8
Devin <devinpeterson10@gmail.com>
2018-10-17 14:25:12 -0400

    Update trim-offers.js

--- dealers/ford/murphy/scripts/trim-offers.js
+++ dealers/ford/murphy/scripts/trim-offers.js
@@ -39,6 +39,32 @@ if(document.getElementById('bit-2018-fusion-se') !== null)  {
             
         });
     });
+    
+  if(document.getElementById('bit-2019-fusion-fwdse') !== null)  {
+    
+    var bitmoSheet = "Fusion";
+    var bitmoRow = "4";
+    var bitmoRange = bitmoSheet+"!A"+bitmoRow+":K"+bitmoRow;
+    var URL = "https://sheets.googleapis.com/v4/spreadsheets/"+spreadsheet+"/values/"+bitmoRange+"?key="+key;
+    
+    console.log(URL);
+    
+    jQuery.getJSON(URL, function(data) {
+        $.each(data.values, function(index,value) {
+            
+            document.getElementById('BitMoto-leaseOffer-2019FusionFWDSE').innerHTML = value[2];
+            document.getElementById('BitMoto-DAS-2019FusionFWDSE').innerHTML = value[3];
+            
+            //document.getElementById('BitMoto-APR-2018FusionSE').innerHTML = value[4];
+            //document.getElementById('BitMoto-termLength-2018FusionSE').innerHTML = value[5];
+            
+            //document.getElementById('BitMoto-buyFor-2018FusionSE').innerHTML = value[7];
+            
+            //document.getElementById('BitMoto-claimButton-2018FusionSE').innerHTML = value[8];
+            //document.getElementById('BitMoto-claimButton-2018FusionSE').href = value[9];
+            
+        });
+    });
 
 }
 
