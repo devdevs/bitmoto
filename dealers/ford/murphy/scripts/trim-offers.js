d85e9d998f9035fbe3e89942f8eaeb4a5af9bf9d
Devin <devinpeterson10@gmail.com>
2018-10-17 14:30:33 -0400

    Update trim-offers.js

--- dealers/ford/murphy/scripts/trim-offers.js
+++ dealers/ford/murphy/scripts/trim-offers.js
@@ -7,21 +7,18 @@ var year = 0;   var trim = 1;   var leaseOffer = 2;    var DAS = 3;    var APR =
 var termLength = 5; var plusCash = 6; var buyFor = 7; var claimOfferText = 8;   var claimOfferLink  = 9;
 var disclaimer = 10;
 
-console.log(spreadsheet);
 
 
 function trimOffer() {
 // sample URL =
 // https://sheets.googleapis.com/v4/spreadsheets/1ifxFfj99eGi017xJVKlh99XWLH4SDChl2oefqh2hbeg/values/Fusion!A3:K3?key=AIzaSyBtj9Bs0ue2TA6PIMbIh5lfFPnPhn-w058
     
-if(document.getElementById('bit-2018-fusion-se') !== null)  {
+  if(document.getElementById('bit-2018-fusion-se') !== null)  {
     
     var bitmoSheet = "Fusion";
     var bitmoRow = "3";
     var bitmoRange = bitmoSheet+"!A"+bitmoRow+":K"+bitmoRow;
     var URL = "https://sheets.googleapis.com/v4/spreadsheets/"+spreadsheet+"/values/"+bitmoRange+"?key="+key;
-    
-    console.log(URL);
     
     jQuery.getJSON(URL, function(data) {
         $.each(data.values, function(index,value) {
@@ -39,7 +36,7 @@ if(document.getElementById('bit-2018-fusion-se') !== null)  {
             
         });
     });
-}
+  }
     
   if(document.getElementById('bit-2019-fusion-fwdse') !== null)  {
     
